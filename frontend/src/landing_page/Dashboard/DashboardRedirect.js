958079112816b8f6dc32db15bb077a3cfaedcfb9
Anuj Kumar <anujkumarbro002@gmail.com>
2026-05-13 11:42:55 +0530

    Add Changes

--- frontend/src/landing_page/Dashboard/DashboardRedirect.js
+++ frontend/src/landing_page/Dashboard/DashboardRedirect.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function DashboardRedirect() {
 
   useEffect(() => {
-    window.location.href = "https://stock-trading-platform-zerodha-dashboard.onrender.com/holdings";
+    window.location.href = "https://stock-trading-platform-zerodha-dashboard.onrender.com/";
   }, []);
 
   return <h1>Redirecting...</h1>;
